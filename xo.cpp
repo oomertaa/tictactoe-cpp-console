10c3b28363f6290d3832a1a3ce134def50e6e01a
oomertaa <111696203+oomertaa@users.noreply.github.com>
2023-11-12 22:29:18 +0200

    tictactoe cpp console v. 1.0
    Suboptimal win() function that checks if a player has won.

---
+++ xo.cpp
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <stdlib.h>
+
+char a[10];
+short int ct = 1;
+
+//Shows the player the current state of the game
+void state(){
+
+   //Announces turn
+   std::cout << "Player ";
+   (ct % 2 == 1) ? std::cout << "X" : std::cout << "O";
+   std::cout <<", it's your turn" << std::endl;
+   
+   //Builds the Tic Tac Toe grid and prints symbols when necessary 
+   for (int i = 0 ; i < 3; i++)
+   {
+      for (int j = 1; j <= 3; j++)
+      {
+         if (a[3*i+j] == 'O' || a[3*i+j] == 'X')
+         {
+            std::cout << " " << a[3*i+j] << " ";
+         }
+         else std::cout <<"   ";
+         if (j != 3) std::cout <<"|";
+      }
+
+      if (i != 2)  //dont print these after the last
+      std::cout << std::endl << "---|---|---" << std::endl;
+      else std::cout << std::endl;
+   }
+}
+
+/*
+Model of the grid:
+
+ X | X | 0
+---|---|---
+ X | 0 | 0
+---|---|---
+   |   |  
+*/
+
+void makeMove()
+{
+   std::cout << "Input your coordinates: ";
+   int x, y;
+   std::cin >> x >> y;
+   
+   //Check validity of coordinates
+   if (x < 1 || x > 3 || y < 1 || y > 3)
+   {
+      std::cout << "The coordinates you have entered are invalid." << std::endl;
+      makeMove();
+      return;
+   }
+
+   //Check availability of coordinates
+   if (a[(x-1)*3 + y] == 'X' || a[(x-1)*3 + y] == 'O')
+   {
+      std::cout << "That space is already taken" << std::endl;
+      makeMove();
+      return;
+   }
+
+   a[(x-1)*3 + y] = (ct % 2 == 1)  ? 'X' : 'O';
+}
+
+short int won()
+{ 
+   char check = (ct % 2 == 1) ? 'X' : 'O';
+
+   for (int i = 0; i < 3 ; i++)
+   {
+      if (a[3*i + 1] == check && a[3*i + 2] == check && a[3*i + 3] == check || a[3*0 + i + 1] == check && a[3*1 + i + 1] == check && a[3*2 + i + 1] == check) 
+      {
+         return (ct % 2 == 1) ? 1 : 2;
+      }
+   }
+   if (a[1] == check && a[5] == check && a[9] == check || a[3] == check && a[5] == check && a[7] == check)
+   {
+      return (ct % 2 == 1) ? 1 : 2;
+   }
+   return 0;
+}
+
+int main()
+{
+   int x;
+   for (int i = 0; i < 4; i++)
+   {
+      
+      state();
+      makeMove();
+      ct++;
+   }
+   for (int i = 0; i < 5; i++)
+   {
+       
+      state();
+      makeMove();
+      if (x = won()) 
+      {
+      state();
+      std::cout << std::endl <<  "Player " << x << " won!";
+      return 0;
+      }
+      ct++;  
+   }
+   state();
+   std::cout << "It's a tie!";
+   return 0;
+}
